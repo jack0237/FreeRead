91b668e6cf33db034c8ebbd79c03bea315cb3cd3
jack0237 <99824627+jack0237@users.noreply.github.com>
2023-11-15 09:23:39 +0100

    First freeRead back with spring

---
+++ FreeReadBack/src/main/java/com/SaintJean/FreeRead/FreeReadApplication.java
@@ -0,0 +1,13 @@
+package com.SaintJean.FreeRead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeReadApplication.class, args);
+	}
+
+}
