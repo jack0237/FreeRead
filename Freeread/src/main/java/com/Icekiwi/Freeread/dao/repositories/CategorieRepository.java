5b7624d9623a640019417ae0825554e3943d0f74
jack0237 <99824627+jack0237@users.noreply.github.com>
2023-12-01 15:12:49 +0100

    .

--- Freeread/src/main/java/com/Icekiwi/Freeread/dao/repositories/CategorieRepository.java
+++ Freeread/src/main/java/com/Icekiwi/Freeread/dao/repositories/CategorieRepository.java
@@ -4,10 +4,18 @@ import com.Icekiwi.Freeread.dao.entities.CategorieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CategorieRepository extends JpaRepository<CategorieEntity, Long> {
-    @Override
-    Optional<CategorieEntity> findById(Long aLong);
+
+    Optional<CategorieEntity> findById(Long id);
+
+    List<CategorieEntity> findByDescription(String description);
+
+    Optional<CategorieEntity> findByNomAndDescription(String nom, String description);
+    List<CategorieEntity> findByDeletedFalse();
+
 }
+
---
+++ Freeread/src/main/java/com/Icekiwi/Freeread/dao/repositories/PretRepository.java
@@ -0,0 +1,8 @@
+package com.Icekiwi.Freeread.dao.repositories;
+
+import com.Icekiwi.Freeread.dao.entities.PretEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PretRepository extends JpaRepository<PretEntity, Long> {
+    
+}
